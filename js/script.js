e9b83b54202fbf2b56530a1f0aa87650721aaa2c
Denys Rudenko <d7455258@gmail.com>
2022-07-20 15:47:18 +0300

    update script

--- js/script.js
+++ js/script.js
@@ -1,13 +1,1 @@
 "use strict";
-
-let str = "ababc";
-let c = 'c';
-
-
-function firstToLast(str,c){
-  let check = str.search(c);
-  if ( check == -1) return check;
-  
-}
-
-console.log(firstToLast(str, c));
